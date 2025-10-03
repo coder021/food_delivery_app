e30d1dff8f929fdb65d767b3e46a807521521696
Pranaav A <58582743+coder021@users.noreply.github.com>
2025-10-03 16:36:54 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import PlaceOrder  from './pages/PlaceOrder/PlaceOrder.jsx'
 import StoreContextProvider from './context/StoreContext.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import LoginPopup from './components/LoginPopup/LoginPopup.jsx'
-import Verify from './pages/Verify/verify.jsx'
+import Verify from './pages/Verify/Verify.jsx'
 import MyOrders from './pages/MyOrders/MyOrders.jsx'
 
 const App = () => {
